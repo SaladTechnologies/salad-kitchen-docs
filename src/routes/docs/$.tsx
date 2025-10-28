a6e9119c504de4e08e6c5909ef6a2b8047044f5d
Kyle Dodson <kyledodson@gmail.com>
2025-10-28 16:28:36 -0700

    Update Fumadocs and Tanstack Start

--- src/routes/docs/$.tsx
+++ src/routes/docs/$.tsx
@@ -1,15 +1,15 @@
+import { docs } from '@/.source'
 import { baseOptions } from '@/lib/layout.shared'
 import { source } from '@/lib/source'
 import { createFileRoute, notFound } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start'
 import { staticFunctionMiddleware } from '@tanstack/start-static-server-functions'
-import type { PageTree } from 'fumadocs-core/server'
+import type * as PageTree from 'fumadocs-core/page-tree'
 import { createClientLoader } from 'fumadocs-mdx/runtime/vite'
 import { DocsLayout } from 'fumadocs-ui/layouts/docs'
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 import { DocsBody, DocsDescription, DocsPage, DocsTitle } from 'fumadocs-ui/page'
 import { useMemo } from 'react'
-import { docs } from '../../../source.generated'
 
 export const Route = createFileRoute('/docs/$')({
   component: Page,
@@ -24,8 +24,8 @@ export const Route = createFileRoute('/docs/$')({
 const loader = createServerFn({
   method: 'GET',
 })
-  .middleware([staticFunctionMiddleware])
   .inputValidator((slugs: string[]) => slugs)
+  .middleware([staticFunctionMiddleware])
   .handler(async ({ data: slugs }) => {
     const page = source.getPage(slugs)
     if (!page) throw notFound()
@@ -67,28 +67,35 @@ function Page() {
   )
 }
 
-function transformPageTree(tree: PageTree.Folder): PageTree.Folder {
-  function transform<T extends PageTree.Item | PageTree.Separator>(item: T) {
-    if (typeof item.icon !== 'string') return item
-
-    return {
-      ...item,
-      icon: (
-        <span
-          dangerouslySetInnerHTML={{
-            __html: item.icon,
-          }}
-        />
-      ),
+function transformPageTree(root: PageTree.Root): PageTree.Root {
+  function mapNode<T extends PageTree.Node>(item: T): T {
+    if (typeof item.icon === 'string') {
+      item = {
+        ...item,
+        icon: (
+          <span
+            dangerouslySetInnerHTML={{
+              __html: item.icon,
+            }}
+          />
+        ),
+      }
     }
+
+    if (item.type === 'folder') {
+      return {
+        ...item,
+        index: item.index ? mapNode(item.index) : undefined,
+        children: item.children.map(mapNode),
+      }
+    }
+
+    return item
   }
 
   return {
-    ...tree,
-    index: tree.index ? transform(tree.index) : undefined,
-    children: tree.children.map((item) => {
-      if (item.type === 'folder') return transformPageTree(item)
-      return transform(item)
-    }),
+    ...root,
+    children: root.children.map(mapNode),
+    fallback: root.fallback ? transformPageTree(root.fallback) : undefined,
   }
 }
